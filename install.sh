f24a659920b1452f14d10683ed00e10a55e7e835
SugarShootingStar <ryu.8224@gmail.com>
2018-11-18 22:36:34 +0900

    feat:zsh spaceship-theme setting fix

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@ for f in .??*
 do
     [ "$f" = ".git" ] && continue
     # ホームディレクトリのdotFiles配下を参照する設定
-    ln -snfv "$HOME""/dotFiles/""$f" "$HOME""/""$f"
+    ln -snfv "$HOME/dotFiles/$f" "$HOME/$f"
 done
+
+# zsh用カスタムテーマを使えるようにする設定
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+
+ln -snfv "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
