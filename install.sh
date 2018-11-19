441ba2abc7ff2b42b3a259fa7820415f166ac515
SugarShootingStar <ryu.8224@gmail.com>
2018-11-19 16:06:11 +0900

    feat:anyenvのインストール追加

--- install.sh
+++ install.sh
@@ -9,3 +9,13 @@ done
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
 ln -snfv "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# anyenvのインストール
+git clone https://github.com/riywo/anyenv ~/.anyenv
+
+## anyenv update plugin
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+
+# シェル再起動
+exec $SHELL -l
