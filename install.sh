3eacc324fa6575d01d47114f1f1c13741c6addfc
ryuta69 <eyma22s.yu@gmail.com>
2020-06-20 15:01:31 +0900

    Fix karabiner symlink target path
    karabiner dir is supposed to be placed in XDG_CONFIG, that is ~/.config for mac.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
+    [ "$f" = "karabiner" ] && ln -snfv "$HOME/dotFiles/$f" "$HOME/.config/$f" && continue
     # ホームディレクトリのdotFiles配下を参照する設定
     ln -snfv "$HOME/dotFiles/$f" "$HOME/$f"
 done
